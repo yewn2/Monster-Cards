a93c6a495af1bc62e327be9de7dbbc495d9f04f4
yewn2 <yewn2@middleton.school.nz>
2024-05-07 17:12:25 +1200

    welcome_instructions version 1

--- 01_welcome_instructions_v1.py
+++ 01_welcome_instructions_v1.py
@@ -10,7 +10,7 @@ import easygui
 
 
 # display welcome screen
-easygui.msgbox("Welcome to Burger Menu Combo Editor!", "Welcome")
+easygui.msgbox("Welcome to Monster Cards!", "Welcome")
 
 # display instructions
 easygui.msgbox("Instructions go here", "Instructions")
