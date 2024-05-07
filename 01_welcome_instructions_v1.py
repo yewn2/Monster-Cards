05268ebda3f11ce2444160ff50ca93c8e41f4d0b
yewn2 <yewn2@middleton.school.nz>
2024-05-07 17:12:55 +1200

    welcome_instructions version 1

--- 01_welcome_instructions_v1.py
+++ 01_welcome_instructions_v1.py
@@ -2,6 +2,7 @@
 Welcome screen and instructions version 1
 Displays a welcome screen with appropriate text/title
 Continues on to display an instruction box
+Copied from previous project: Burger Menu Combos
 """
 
 
