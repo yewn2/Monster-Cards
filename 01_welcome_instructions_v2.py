57fcdcc44d920ef06589b1eb9cf53c729f2d2583
yewn2 <yewn2@middleton.school.nz>
2024-05-07 17:15:07 +1200

    welcome_instructions version 2

---
+++ 01_welcome_instructions_v2.py
@@ -0,0 +1,34 @@
+"""
+Welcome screen and instructions version 2
+Takes welcome_instructions v1 and places it into a function for easier use
+Instructions and program name variables added for easier function use in future
+Copied from previous project: Burger Menu Combos
+"""
+
+# imports
+import easygui
+
+
+def welcome_instructions(program_name, instruction_list):
+    # display welcome screen
+    easygui.msgbox(f"Welcome to {program_name}!", "Welcome")
+
+    # display instructions
+    instructions = [f"Instructions for use of {program_name}\n"]
+    for inst in instruction_list:
+        instructions.append(f"\n* {inst} *")
+    easygui.msgbox("".join(instructions), "Instructions")
+
+
+# Main routine goes here:
+welcome_instructions("Monster Cards",
+                     ["Choose an action after this box",
+                      "To add a card: enter a card ID, then fill in the stats "
+                      "for the card, then confirm "
+                      "your entry.",
+                      "To delete a card, enter the card ID and confirm your "
+                      "choice.",
+                      "To find a card, enter the specific card ID.",
+                      "'Output all' allows you to see which card are already "
+                      "present in the menu.",
+                      "Once you have finished, exit the program."])
