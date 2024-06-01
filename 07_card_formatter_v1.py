cc854733aecffff57ab315c6cb150571c3a9fd12
yewn2 <yewn2@middleton.school.nz>
2024-06-01 13:19:34 +1200

    card_formatter version 1

---
+++ 07_card_formatter_v1.py
@@ -0,0 +1,11 @@
+"""
+Card formatter version 1
+This component adds formatting around the cards that are outputted,
+giving the feel of a systematic layout.
+"""
+
+
+def card_formatter(card):
+    print("-" * 100)
+    print(card)
+    print("-" * 100)
