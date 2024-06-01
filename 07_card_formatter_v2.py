7dfd3d869c343d1521bc2395646574fc678c7352
yewn2 <yewn2@middleton.school.nz>
2024-06-01 13:39:26 +1200

    card_formatter version 2

---
+++ 07_card_formatter_v2.py
@@ -0,0 +1,19 @@
+"""
+Card formatter version 2
+More code added, such as emojis around the characteristics of the monster card.
+"""
+
+
+def card_formatter(card):
+    # finds the different stats and adds emojis to the card
+    index = card.find("Strength")
+    emoji_card = card[:index] + "💪" + card[index:]
+    index = emoji_card.find("Speed")
+    emoji_card = emoji_card[:index] + "🏃" + emoji_card[index:]
+    index = emoji_card.find("Stealth")
+    emoji_card = emoji_card[:index] + "🥷" + emoji_card[index:]
+    index = card.find("Cunning")
+    emoji_card = emoji_card[:(index + 3)] + "🧠" + emoji_card[(index + 3):]
+    print("~" * 100)
+    print(emoji_card)
+    print("~" * 100)
